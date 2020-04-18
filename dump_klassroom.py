8c63fc4c164f23ca0d7b657510028dfdbb2ae9e8
Ugrash <mops@ugrash.org>
2020-04-18 11:58:28 +0200

    Get Students and Users

--- dump_klassroom.py
+++ dump_klassroom.py
@@ -8,6 +8,32 @@ import re
 logging.basicConfig(level=logging.INFO)
 
 
+class Student:
+    def __init__(self, student, users):
+        logging.info('Student __init__')
+        self.first_name = student['first_name']
+        self.last_name = student['last_name']
+        self.family = ', '.join([f'{v}: {users[k]["name"]}' for k, v in student['members'].items()])
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.family})'
+
+
+class Klass:
+    def __init__(self, klass, users):
+        logging.info('Klass __init__')
+        self.created_at = klass['created_at']
+        self.id = klass['id']
+        self.level = klass['level']
+        self.name = klass['natural_name']
+        self.school = klass['school']['name']
+        self.students = [Student(s, users) for s in klass['students'].values()]
+
+    def __str__(self):
+        students_list = "\n".join([str(s) for s in self.students])
+        return f'{self.name} {self.school} ({self.level})\n{students_list}'
+
+
 class KlassRoom:
     def __init__(self):
         logging.info('__init__')
@@ -17,9 +43,12 @@ class KlassRoom:
         self.web_device = None
         self.app_id = None
         self.auth_token = 'null'
+        self.users = {}
+        self.klasses = {}
 
     def post_data(self):
-        return {'device': self.web_device,
+        return {'auth_token': self.auth_token,
+                'device': self.web_device,
                 'app_id': self.app_id,
                 'version': '4.0',
                 'culture': 'fr',
@@ -36,16 +65,16 @@ class KlassRoom:
         logging.info(f'app_id: {self.app_id}')
 
     def app_connect(self):
-        post_data = {'auth_token': self.auth_token}
-        post_data.update(self.post_data())
+        post_data = self.post_data()
         response = self.session.post(f'https://{self.api_url}/app.connect',
                                      data=post_data)
-        return response.json()
+        if 'klasses' in response.json():
+            self.klasses = response.json()['klasses']
+            self.users = response.json()['users']
 
     def auth(self, phone, password):
         post_data = {'phone': phone,
-                     'password': password,
-                     'auth_token': self.auth_token}
+                     'password': password}
         post_data.update(self.post_data())
         response = self.session.post(f'https://{self.api_url}/auth.basic',
                                      data=post_data)
@@ -57,4 +86,7 @@ if __name__ == '__main__':
     kr.init_klassroom_session()
     kr.app_connect()
     kr.auth(*sys.argv[1:3])
-    pprint.pprint(kr.app_connect())
+    kr.app_connect()
+    for klass_key, klass in kr.klasses.items():
+        kklass = Klass(klass, kr.users)
+        print(kklass)
